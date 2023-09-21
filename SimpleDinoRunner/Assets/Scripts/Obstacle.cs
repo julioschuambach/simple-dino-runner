bcf05bafc4d3fa12fd79fbf639c7c44693d1f4d6
julioschuambach <julioschuambach.dev@gmail.com>
2023-09-21 00:10:48 -0300

    feat: implemented score and highscore

--- SimpleDinoRunner/Assets/Scripts/Obstacle.cs
+++ SimpleDinoRunner/Assets/Scripts/Obstacle.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Obstacle : MoveObject
 {
+    private Player player;
+    [SerializeField] private bool scored;
+    [SerializeField] private int scoreValue;
     [SerializeField] private GameObject[] obstaclesPrefab;
     private GameObject instantiatePrefab
     {
@@ -11,8 +14,29 @@ public class Obstacle : MoveObject
         }
     }
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        player = FindObjectOfType<Player>();
+    }
+
     protected override GameObject GetInstantiatePrefab()
     {
         return instantiatePrefab;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (transform.position.x < player.transform.position.x && !scored)
+            Score();
+    }
+
+    private void Score()
+    {
+        scored = true;
+        GameManager.instance.Score(scoreValue);
     }
 }
